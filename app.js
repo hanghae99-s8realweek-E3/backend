b83ebc6b8a4695fced3a5c3d66bfa4b3fafb9509
swh_95 <tjdngur22@gmail.com>
2022-08-27 17:09:14 +0900

    start

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+
+const Http = require("http");
+// const Https = require("https");
+const cookieParser = require("cookie-parser");
+const indexRouter = require("./routes");
+
+//로그 관리를 위해 morgan 설치
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+//보안과 가독성을 위해 환경변수사용
+require("dotenv").config();
+
+const http = Http.createServer(app);
+// const https = Https.createServer(options, app);
+
+const http_port = process.env.HTTP_PORT || 3000;
+// const https_port = process.env.HTTPS_PORT || 443;
+
+// const port = process.env.Port;
+
+const cors = require("cors");
+const corsOption = {
+  origin: true,
+  credentials: true,
+};
+
+app.use(cors(corsOption));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+
+http.listen(http_port, () => {
+  console.log(`🟢 ${http_port} 포트로 서버가 열렸어요!`);
+});
+
+// https.listen(https_port, () => {
+//   console.log(`Start listen Server: ${https_port}`);
+// });
---
+++ middlewares/auth_middlewares.js
@@ -0,0 +1,50 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("../models");
+// const MYSQL_KEY = process.env.MYSQL_KEY;
+
+module.exports = (req, res, next) => {
+  // Client 요청의 cookies 객체 중 토큰을 authorization으로 읽어들여서, 공백을 기준으로 두 조각으로 나눔
+  const { authorization } = req.headers;
+  // console.log(authorization, "인증확인");
+  const [authType, authToken] = (authorization || "").split(" ");
+
+  // 전달받은 인증값이 Bearer로 시작하지 않으면 인증 실패
+  if (authType !== "Bearer") {
+    res.status(401).json({
+      errorMessage: "로그인 후 사용해주세요, Bearer 토큰이 아님",
+    });
+    return;
+  }
+  try {
+    // 뒤쪽 'authToken'을 우리 MYSQL_KEY를 가지고 인증해보고 에러 없으면, user 정보를 토근으로 다음 next으로 넘겨줌
+    jwt.verify(
+      authToken,
+      process.env.MYSECRET_KEY,
+
+      async (error, decoded) => {
+        // 인증 결과 에러가 나타나면 클라이언트와 서버에 모두 에러를 던지고 미들웨어 종료
+        if (error) {
+          res.status(401).send({
+            errorMessage:
+              "이용에 문제가 있습니다. 관리자에게 문의해주세요, 토큰 인증 실패",
+          });
+          // console.error(error);
+          return;
+        }
+
+        // 에러없이 잘 인증 된거면, 인증된 사용자이므로 decoding 된 decode 객체가 생김
+        // 이 decoded 객체로 DB로부터 사용자 정보를 빼 와서 토큰을 res.locals(전역 객체) 위치에 반환
+        let user = await User.findOne({ where: { userId: decoded.userId } });
+
+        res.locals.user = user;
+        console.log(res.locals.user);
+        next();
+      }
+    );
+  } catch (e) {
+    res.status(401).send({
+      errorMessage: "로그인 후 사용하세요.(2) 토큰 검증 불가",
+    });
+    return;
+  }
+};
