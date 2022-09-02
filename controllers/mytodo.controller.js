f9a653bc477ad92bccf14f6c97806f6af2177399
Jiyu Jang <apryll7995@gmail.com>
2022-09-03 04:12:05 +0900

    Add(todo API - 나의 todo 피드 조회, 타인의 todo 피드 조회 구현)

--- controllers/mytodo.controller.js
+++ controllers/mytodo.controller.js
@@ -4,7 +4,33 @@ class MyTodoController {
   myTodoService = new MyTodoService();
 
   // 나의 todo 피드 조회 [GET] /api/mytodos
+  getMyTodo = async (req, res, next) => {
+    try {
+      const { user } = res.locals;
+      const { date } = req.query;
+
+      const data = await this.myTodoService.getMyTodo(user, date);
+
+      res.status(200).json({ message: "success", data });
+    } catch (err) {
+      next(err);
+    }
+  };
+
   // 타인의 todo 피드 조회 [GET] /api/mytodos/:userId
+  getUserTodo = async (req, res, next) => {
+    try {
+      const { user } = res.locals;
+      const { userId } = req.params;
+
+      const data = await this.myTodoService.getUserTodo(user, userId);
+
+      res.status(200).json({ message: "success", data });
+    } catch (err) {
+      next(err);
+    }
+  };
+
   // 오늘의 제안 todo 작성 [POST] /api/mytodos
   // todo 삭제 [DELETE] /api/mytodos/:todoId
   // 오늘의 도전 todo 등록 [POST] /:todoId/challenged
