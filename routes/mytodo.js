5cae9805379893a88dbe8dbe6e145cf99f6ecabb
Jiyu Jang <apryll7995@gmail.com>
2022-09-03 04:08:04 +0900

    Fix(주석 수정)

--- routes/mytodo.js
+++ routes/mytodo.js
@@ -7,27 +7,27 @@ const nonUserMiddleware = require("../middlewares/nonUser_middlewares");
 
 const myTodoController = new MyTodoController();
 
-// 나의 todo 피드 조회
+// 나의 todo 피드 조회 [GET] /api/mytodos
 router.get("/", authMiddleware, myTodoController.getMyTodo);
-// 타인의 todo 피드 조회
+// 타인의 todo 피드 조회 [GET] /api/mytodos/:userId
 router.get("/:userId", nonUserMiddleware, myTodoController.getUserTodo);
-// 오늘의 제안 todo 작성
+// 오늘의 제안 todo 작성 [POST] /api/mytodos
 router.post("/", authMiddleware, myTodoController.createTodo);
-// todo 삭제
+// todo 삭제 [DELETE] /api/mytodos/:todoId
 router.delete("/:todoId", authMiddleware, myTodoController.deleteTodo);
-// 오늘의 도전 todo 등록
+// 오늘의 도전 todo 등록 [POST] /:todoId/challenged
 router.post(
   "/:todoId/challenged",
   authMiddleware,
   myTodoController.createChallengedTodo
 );
-// 오늘의 도전 todo 완료/진행중 처리
+// 오늘의 도전 todo 완료/진행중 처리 [PUT] /:todoId/challenged
 router.put(
   "/:todoId/challenged",
   authMiddleware,
   myTodoController.completeChallengedTodo
 );
-// 오늘의 도전 todo 등록 취소
+// 오늘의 도전 todo 등록 취소 [DELETE] /:todoId/challenged
 router.delete(
   "/:todoId/challenged",
   authMiddleware,
