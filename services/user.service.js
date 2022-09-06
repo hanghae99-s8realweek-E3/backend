e316dac9c6a562d9c739cc676583839db906b3ac
Jiyu Jang <apryll7995@gmail.com>
2022-09-07 02:07:37 +0900

    Fix(이메일 인증 메일 발송 내용 변경)

--- services/user.service.js
+++ services/user.service.js
@@ -150,7 +150,13 @@ class UserService {
       to: email, // list of receivers
       subject: "MIMIC 이메일 인증", // Subject line
       text: "MIMIC", // plain text body
-      html: `<h2><b>당신의 하루, 나의 하루. MIMIC</b></h2><b>인증번호는 ${authNumber} 입니다</b><p>본 인증 번호는 2시간 동안만 유효합니다.</p>`, // html body
+      html: `<h1>[ MIMIC 이메일 인증 안내 ]</h1>
+              <h2><sup>당신의 하루, 나의 하루. MIMIC🎯</sup></h2>
+              <p>안녕하세요. MIMIC 이메일 인증을 위한 메일입니다.</p>
+              <p>인증 번호는 1시간 후에 만료됩니다. 1시간 내로 입력해 주시기 바랍니다.</p>
+              <h3>• 인증번호</h3><p>
+              <strong><span style="color: rgb(53, 152, 219);" data-mce-style="color: #3598db;">${authNumber}</span></strong></p>
+              <p><sub>∗ 인증 번호 유효 시간이 지난 경우, 다시 신청 후 진행해 주세요.</sub></p>`,
     };
 
     const transporter = nodemailer.createTransport(configOptions);
