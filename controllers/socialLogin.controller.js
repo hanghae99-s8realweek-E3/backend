0e345f3d5ecab9684fcc9699dbf49cf55486e7fa
Jiyu Jang <apryll7995@gmail.com>
2022-09-13 13:17:36 +0900

    Fix(카카오 로그인 응답값 변경)

--- controllers/socialLogin.controller.js
+++ controllers/socialLogin.controller.js
@@ -1,23 +1,23 @@
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
+require("dotenv").config();
 
 //Kakao callback Controller======ok
 exports.kakaologin = (req, res, next) => {
-    passport.authenticate(
-        'kakao',
-        { failureRedirect: '/' },
-        (err, user, info) => {
-            if (err) return next(err);
-            const { userId, mbti, nickname } = user;
-            const token = jwt.sign({ userId, mbti, nickname }, process.env.MYSECRET_KEY, {
-                expiresIn: '2d',
-            });
-            // res.redirect(`https://weat.site?token=${token}`);
-            res.status(200).json({
-                token,
-                message: "success",
-            })
-        }
-    )(req, res, next);
+  passport.authenticate(
+    "kakao",
+    { failureRedirect: "/" },
+    (err, user, info) => {
+      if (err) return next(err);
+      const { userId, mbti, nickname } = user;
+      const token = jwt.sign(
+        { userId, mbti, nickname },
+        process.env.MYSECRET_KEY,
+        { expiresIn: "2d" }
+      );
+      res.redirect(
+        `https://mimic-hanghaee99team3.vercel.app/mbti?token=${token}`
+      );
+    }
+  )(req, res, next);
 };
